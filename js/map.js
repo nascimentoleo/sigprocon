708048a46fe025f2d341448d21c5c2b78965a7d7
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2017-03-26 13:04:02 -0300

    Mapa finalizado

--- js/map.js
+++ js/map.js
@@ -1,42 +1,26 @@
-var iconStyle = new ol.style.Style({
-      image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-        scale: 0.1,
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        opacity: 0.75,
-        src: 'data/images/map-icon.png'
-      }))
-    });
-
-    var layerJSONUnits = new ol.layer.Vector({
-        source: new ol.source.Vector({
-        url: 'data/geojson/units.geojson',
-        format: new ol.format.GeoJSON({
-            defaultDataProjection :'EPSG:4326', 
-            projection: 'EPSG:3857'
-
-          })
-        }),
-        style : iconStyle
-    });
-
-/*    var layerJSONComplaints = new ol.layer.Vector({
-        source: new ol.source.Vector({
-        url: 'data/geojson/ceps1_mapped.geojson',
-        format: new ol.format.GeoJSON({
-            defaultDataProjection :'EPSG:4326', 
-            projection: 'EPSG:3857'
-
-          })
-        })
-    }); */
-
- var layerJSONComplaints = new ol.source.Vector({
-    url: 'data/geojson/ceps1_mapped.geojson',
-    format: new ol.format.GeoJSON(),
-    style : iconStyle
+var layerJSONComplaints = new ol.source.Vector({
+  url: 'data/geojson/ceps_locations_repeated.geojson',
+  format: new ol.format.GeoJSON()
 });
+
+var layerProcons = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'data/geojson/procons_ma.geojson',
+    format: new ol.format.GeoJSON()
+  }),
+  style : new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 8,
+      stroke: new ol.style.Stroke({
+        color: '#fff'
+      }),
+      fill: new ol.style.Fill({
+        color: '#FFA500'
+      })
+    })
+  })
+});
+
 var clusterSource = new ol.source.Cluster({
   distance: 40,
   source: layerJSONComplaints
@@ -75,16 +59,16 @@ var clusters = new ol.layer.Vector({
 
 
 var raster = new ol.layer.Tile({
-    source: new ol.source.OSM({})
+  source: new ol.source.OSM({})
 });
 
 
 var map = new ol.Map({
-   layers: [raster, clusters],
-   renderer: 'canvas',
-   target: 'map',
-   view: new ol.View({
-     center: ol.proj.fromLonLat([-45.475006, -5.801333]),
-     zoom: 6.3
-   })
+  layers: [raster, clusters, layerProcons],
+  renderer: 'canvas',
+  target: 'map',
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-45.475006, -5.801333]),
+    zoom: 6.3
+  })
 }); 
